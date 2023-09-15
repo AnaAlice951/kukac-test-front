d224fe860410d71402d7149af92ef24400956d21
Ana Alice <aasoares55@gmail.com>
2023-09-15 15:23:07 -0300

    Fixing Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,23 +9,23 @@ export function Home() {
       <Header />
       <div className="w-full h-full font-architectsDaughter flex justify-between items-start p-10 lg:p-20 overflow-y-auto">
         <div className="flex flex-col h-full gap-5 lg:gap-20 w-full justify-center">
-          <p className="font-bold text-[70px] lg:text-[90px] text-black_75">
+          <p className="font-bold text-[50px] lg:text-[80px] text-black_75">
             Teste Técnico Kukac
           </p>
           <p className="font-bold text-[30px] lg:text-[35px]  text-black_75">
             by Ana Alice
           </p>
-          <div className="flex flex-row gap-5 w-5 h-5">
+          <div className="flex flex-row lg:gap-20 gap-10 mt-5">
             <a
               href={instagramURL}
               className="h-3 w-9"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <BsInstagram />
+              <BsInstagram className="lg:w-10 lg:h-10 w-7 h-7" />
             </a>
             <a href={gitHubURL} target="_blank" rel="noopener noreferrer">
-              <BsGithub />
+              <BsGithub className="lg:w-10 lg:h-10 w-7 h-7" />
             </a>
           </div>
         </div>
